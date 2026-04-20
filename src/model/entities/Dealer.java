5a58fabdccb29db2ce965508eebfb9f5bbaa1b70
Francesco Pio Venturi <f.venturi7@campus.uniurb.it>
2026-04-20 11:10:00 +0200

    dealer logic implementation

--- src/model/entities/Dealer.java
+++ src/model/entities/Dealer.java
@@ -1,12 +1,16 @@
 package model.entities;
 
 import model.game.Hand;
+import model.cards.Deck;
 
 public class Dealer extends Participant{
 
-    public Dealer()
-    {
+    private final Deck deck;
+
+    public Dealer(Deck deck) {
         super("Dealer");
+        this.deck = deck;
+
     }
 
     // override
@@ -15,5 +19,19 @@ public class Dealer extends Participant{
     {
         if (super.getHands().isEmpty())
             super.addHand(hand);
+    }
+    // false continua a giocare
+    // true ha finito
+    public boolean play() {
+        if (super.getHands().getFirst().getValue() <= 16)
+            super.getHands().getFirst().addCard(deck.getCard());
+        return super.getHands().getFirst().getValue() > 16;
+
+    }
+
+    public int getValue() {
+
+        return getHands().getFirst().getValue();
+
     }
 }
